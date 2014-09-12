0de5c944939577bf5809bf2da331e1b1833c7d64
David Zoller <david.l.zoller@gmail.com>
2014-09-11 22:09:02 -0500

    added code for cacheMatrix
    Added R code for cacheMatrix but did not add comments yet.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m_inv <- matrix()
+  set <- function(y) {
+    x <<- y
+    m_inv <<- matrix()
+  }
+  get <- function() x
+  setm_inv <- function(solve_m) m_inv <<- solve_m
+  getm_inv <- function() m_inv
+  list(set = set, get = get,
+       setm_inv = setm_inv,
+       getm_inv = getm_inv)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m_inv <- x$getm_inv()
+  if (!is.na(m_inv[1,1])) {
+    message("getting cached data")
+    return(m_inv)
+  }
+  data <- x$get()
+  m_inv <- solve(data)
+  x$setm_inv(m_inv)
+  m_inv
 }
